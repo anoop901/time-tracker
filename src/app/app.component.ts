bfd44f8f0bc2347691c0a492080a5c92c3126439
Anoop Naravaram <anoop901@gmail.com>
2022-01-25 01:21:25 -0800

    tasks and focuses display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
+import humanizeDuration from 'humanize-duration';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  tasks = [
+    { id: 'a', name: 'Task A' },
+    { id: 'b', name: 'Task B' },
+    { id: 'c', name: 'Task C' },
+  ];
+  focuses = [
+    {
+      task: 'a',
+      start: new Date('2022-01-24T13:00'),
+      end: new Date('2022-01-24T13:24'),
+    },
+    {
+      task: 'a',
+      start: new Date('2022-01-24T13:30'),
+      end: new Date('2022-01-24T13:55'),
+    },
+    {
+      task: 'b',
+      start: new Date('2022-01-24T14:00'),
+      end: new Date('2022-01-24T14:26'),
+    },
+  ];
+
+  focusesForTask(taskId: string) {
+    return this.focuses.filter((focus) => focus.task === taskId);
+  }
+
+  durationOfFocusHumanized(focus: { end: Date; start: Date }) {
+    return humanizeDuration(focus.end.getTime() - focus.start.getTime());
+  }
+}
